b1e1c083f6a90c1824dd615b2982d76fa994120a
CS Team Room 1 <uacacmweb@gmail.com>
2019-02-05 12:03:49 -0600

    added userAdded function

--- officehours-api/functions/src/index.ts
+++ officehours-api/functions/src/index.ts
@@ -1,4 +1,7 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -11,3 +14,15 @@ export const helloWorld = functions.https.onRequest((request, response) => {
      response.send({"message": "Hello there!"});
  })
 });
+
+export const userAdded = functions
+    .auth
+    .user().onCreate((user, context) => {
+        const userRef = db.doc(`users/${user.uid}`);
+
+        return userRef.set({
+            name: user.email,
+            uid: user.uid,
+            dateCreated: context.timestamp
+        });
+    });
